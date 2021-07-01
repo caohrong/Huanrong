a82b2f59fb465f9560453a686bc047a422b83b3e
Huanrong Cao <caohrong@live.com>
2021-07-01 16:22:26 +0800

    dd

---
+++ PDF/nicks-cors-test.js
@@ -0,0 +1,26 @@
+function main() 
+{
+    console.log("main function");
+    console.log("ajax request to the resource which will require cors enabled");
+    $.ajax
+    ({
+        dataType: "json",
+        url: "https://api.github.com",
+        success: function(data) 
+        {
+            console.log("log response on success");
+            console.log(data);
+        }
+    });
+    $.ajax
+    ({
+        dataType: "json",
+        url: "http://mptest.citicbank.com/mstep/wechastream.do?act=filetoPdfWeChat",
+        type: "POST"
+        success: function(data)
+        {
+            console.log("log response on success");
+            console.log(data);
+        }
+    });
+}
