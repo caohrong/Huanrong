03ce5d633711950180545a0fc450061f2e9d905c
Huanrong Cao <caohrong@live.com>
2021-07-01 17:18:35 +0800

    Update nicks-cors-test.js

--- PDF/nicks-cors-test.js
+++ PDF/nicks-cors-test.js
@@ -1,3 +1,39 @@
+function main1() {
+    function success(text) {
+        var textarea = document.getElementById('test-response-text');
+        textarea.value = text;
+    }
+
+    function fail(code) {
+        var textarea = document.getElementById('test-response-text');
+        textarea.value = 'Error code: ' + code;
+    }
+
+    var request = new XMLHttpRequest(); // 新建XMLHttpRequest对象
+
+    request.onreadystatechange = function () { // 状态发生变化时，函数被回调
+        if (request.readyState === 4) { // 成功完成
+            // 判断响应结果:
+            if (request.status === 200) {
+            // 成功，通过responseText拿到响应的文本:
+                return success(request.responseText);
+            } else {
+                // 失败，根据响应码判断失败原因:
+                return fail(request.status);
+            }
+        } else {
+            // HTTP请求还在继续...
+        }
+    }
+
+    // 发送请求:
+    request.open('GET', 'https://api.github.com');
+    request.send();
+
+    alert('请求已发送，请等待响应...');
+
+}
+
 function main() 
 {
     console.log("main function");
