8c6df69d6565782452e171b299481248cc8e2a0f
Huanrong <caohrong@live.com>
2022-08-05 20:38:24 +0800

    2022-08-05 20:38:24

--- Blog_Upload.sh
+++ Blog_Upload.sh
@@ -4,22 +4,21 @@ export PATH
 SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
 TIME=$(date "+%Y-%m-%d %H:%M:%S")
 echo ${SHELL_FOLDER}
-Volumes=${SHELL_FOLDER%/MWebLibrary/*}
+Volumes="$(dirname "$SHELL_FOLDER")"
 echo ${Volumes}
 ImageFolder=${Volumes}"/HRBlog/Album"
-BlogFolder=${Volumes}"/MWebLibrary/HRBlog"
+BlogFolder=${Volumes}"/MWebLibrary/HRBlog/Hexo"
 HTMLFolder=${Volumes}"/HRBlog"
 echo ${ImageFolder}
 
 echo "${python_version}"
+# echo "--------------------[处理图片]"
+# echo ${ImageFolder}
+# cd ${ImageFolder}
 
-echo "--------------------[处理图片]"
-#echo ${ImageFolder}
-#cd ${ImageFolder}
-#
-#source ~/miniconda3/bin/activate HR
-#python --version
-#python tool.py
+# source ~/miniconda3/bin/activate HR
+# python --version
+# python tool.py
 
 echo "--------------------[博客生成]"
 echo ${BlogFolder}
